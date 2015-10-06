2f4ad345053e4b6ffbf4d8e09eb63ef1732d61ab
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-10-06 17:44:19 -0400

    add code for riding

--- app/controllers/ridings_controller.rb
+++ app/controllers/ridings_controller.rb
@@ -1,2 +1,30 @@
 class RidingsController < ApplicationController
+  def index
+    @ridings = Riding.all
+  end
+
+  def show
+    @riding = Riding.find(params[:id])
+    @previous = previous_riding
+    @next = next_riding
+  end
+
+  def previous_riding
+    @ridings = Riding.order("name")
+    @previous = @ridings[@ridings.find_index(
+      Riding.find(params[:id])) - 1
+    ]
+  end
+
+  def next_riding
+    @ridings = Riding.order("name")
+    unless @ridings[@ridings.find_index(Riding.find(params[:id])) + 1].nil?
+      @next = @ridings[@ridings.find_index(
+        Riding.find(params[:id])) + 1
+      ]
+    else
+      @next = @ridings.first
+    end
+  end
+
 end
